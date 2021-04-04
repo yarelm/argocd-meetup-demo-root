9edd70fd41c658cc77a84eae1982ba0f3a48d674
Yarel Maman <yarel@doit-intl.com>
2021-04-04 14:29:04 +0300

    add run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+kubectl apply -f projects/demo-project-dev.yaml
+kubectl apply -f apps/demo-service-dev.yaml
+
+kubectl apply -f projects/demo-project-prod.yaml
+kubectl apply -f apps/demo-service-prod.yaml
